3671bc54b60e0d14bb3236234ccf7c9e1e2be9e4
Mina.Nadii <mina.nady@me.com>
2025-03-04 21:20:57 +0200

    xx

--- src/Components/Sections/Form/Form.jsx
+++ src/Components/Sections/Form/Form.jsx
@@ -1,7 +1,33 @@
 const Form = () => {
   return (
     <>
-      <div className=" "></div>
+      <div className="flex  ">
+        <div className="lg:w-1/2  lg:pl-50">
+          <form action="">
+            <div className=" flex flex-col  gap-4">
+              <input
+                type="text"
+                placeholder="Enter your email"
+                className="w-full p-3 bg-[#CEDCFF] border border-gray-300 rounded-3xl"
+              />
+              <input
+                type="text"
+                placeholder="Enter your email"
+                className="w-full p-3 bg-[#CEDCFF] border border-gray-300 rounded-3xl"
+              />
+              <input
+                type="text"
+                placeholder="Enter your email"
+                className="w-full bg-[#CEDCFF] p-3 border border-gray-300 rounded-3xl"
+              />
+              <button className=" bg-mainColor text-white p-3 rounded-lg">
+                Subscribe
+              </button>
+            </div>
+          </form>
+        </div>
+        <div></div>
+      </div>
     </>
   );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,3 @@
-import { Form } from "react-router-dom";
 import Framebg from "../../assets/Frame.png";
 import HeroCar from "../../assets/HeroCar.png";
 import CarsCards from "../../Components/Sections/Cars/CarSection";
@@ -6,13 +5,14 @@ import Experience from "../../Components/Sections/Experience/Experience";
 import Testimonials from "../../Components/Sections/Testimonials/Testimonials";
 import Work from "../../Components/Sections/Work/WorkSection";
 import Conntent from "./Content";
+import Form from "../../Components/Sections/Form/Form";
 
 const Home = () => {
   return (
     <>
       <section className="absolute top-0 left-0 h-3/4 w-full z-0">
         <div
-          className="h-full w-full bg-no-repeat bg-contain bg-[position:0_0] sm:bg-[position:top_right] relative flex justify-center items-center lg:justify-start z-0"
+          className="h-full w-full  bg-no-repeat bg-contain bg-[position:0_0] sm:bg-[position:top_right] relative flex justify-center items-center lg:justify-start z-0"
           style={{ backgroundImage: `url(${Framebg})` }}
         >
           <img
@@ -42,7 +42,7 @@ const Home = () => {
         <Testimonials />
       </section>
       {/* Form Section */}
-      <section className=" mt-[1rem] w-full h-scre ">
+      <section className=" mt-[1rem] w-full  ">
         <Form />
       </section>
     </>
